3e1ba56f49c7133a7568752be3ad1f66125329a5
likewater <lakebayomackway@gmail.com>
2017-03-04 20:44:00 -0800

    add storage array to backend logic, further seperated backend from frontend.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
+//-----variables-----
+var storageArray = [];
+
 //-----backend logic-----
 function countNumbers(counter) {
-  console.log("counter: " + counter);
-  for (var i = 1; i <= counter; i++) {
+  for ( var i = 1 ; i <= counter; i++) {
     var f = i % 3 == 0, b = i % 5 == 0;
-    //console.log(f ? b ? "Ping-Pong" : "Ping" : b ? "Pong" : i);
-    var results = (f ? b ? "Ping-Pong" : "Ping" : b ? "Pong" : i);
+    storageArray.push(f ? b ? "Ping-Pong" : "Ping" : b ? "Pong" : i);
   }
-  return results;
 }
 
 function resetFunction() {
@@ -15,36 +15,12 @@ function resetFunction() {
 
 //-----front end logic-----
 $(function() {
-    $("#formFlow").submit(function() {
+  $("#formFlow").submit(function() {
     event.preventDefault();
     var userInput = $("#textInput").val();
     gameList = countNumbers(userInput);
-    $("#listOutput").append('<li>' + gameList + '</li>');
+    storageArray.forEach(function(listItems) {
+      $("#listOutput").append('<li>' + listItems + '</li>');
+    });
   });
 });
-
-
-//-----old original working code-----
-  // //backend logic
-  // $(function() {
-  //   var count = function countNumbers(counter) {
-  //     for (var i = 1; i <= counter; i++) {
-  //       var f = i % 3 == 0, b = i % 5 == 0;
-  //       console.log(f ? b ? "Ping-Pong" : "Ping" : b ? "Pong" : i);
-  //       //Thanks Paul Irish!
-  //       $("#listOutput").append('<li>' + (f ? b ? "Ping-Pong" : "Ping" : b ? "Pong" : i) + '</li>');
-  //     }
-  //     return result;
-  //   }
-  //
-  // //front end logic
-  //   $("#formFlow").submit(function() {
-  //     event.preventDefault();
-  //     var userInput = $("#textInput").val();
-  //     result = count(userInput);
-  //   });
-  // });
-  //
-  // function resetFunction() {
-  //   location.reload();
-  //   }
